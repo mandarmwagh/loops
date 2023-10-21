f6748e778fdb7dda76a5f1caff988dffd7254876
mandarmwagh <mandarmwagh@gmail.com>
2023-10-21 10:53:45 +0530

    first commit

---
+++ src/com/prowing/java/RecursiveMethod.java
@@ -0,0 +1,25 @@
+package com.prowing.java;
+
+public class RecursiveMethod {
+
+	public static void main(String[] args) {
+
+		int i = 1;
+		int n = 10;
+
+		int factorial = factorial(5);
+
+		System.out.println("Factorial is : " + factorial);
+	}
+
+	public static int factorial(int n) // recursive method
+	{
+		if (n == 0) {
+			return 1;
+		} else {
+			// Recursive case: factorial(n) = n * factorial(n-1)
+			return n * factorial(n - 1);
+		}
+	}
+
+}
